2b346991a2ae4c6c3ec9a6ae8368a7cc430687a9
Stephen Barnes <stephen6174@gmail.com>
2021-10-17 07:27:10 -0500

    Update, add more MLG saves

--- src/main/java/com/stebars/nobucketshenanigans/OptionsHolder.java
+++ src/main/java/com/stebars/nobucketshenanigans/OptionsHolder.java
@@ -19,15 +19,34 @@ public class OptionsHolder {
 		
 		public Common(ForgeConfigSpec.Builder builder) {			
 	        itemsCancelRightClick = builder.comment("List of all the items you want to block right-clicking with while falling. "
-	        		+ "To block bucket saving in vanilla, you only need water and lava buckets (not e.g. milk buckets because "
-	        		+ "they can't be placed). If you have a mod adding milk, honey, slime, etc. then add their items here. "
-	        		+ "This includes e.g. teapots, cups, bowls that can place fluids. You can also put non-fluid-placing items "
-	        		+ "in here, e.g. flint and steel, though I only tested this for flint and steel.")
+	        		+ "If you're using mods, add their items here. "
+	        		+ "This includes e.g. teapots, cups, bowls that can place fluids.")
 	        		.define("itemsBlockRightClick", Lists.newArrayList(
 	        				"minecraft:water_bucket",
 	        				"minecraft:lava_bucket",
 	        				"minecraft:hay_block",
-	        				"minecraft:cobwebs"));
+	        				"minecraft:cobwebs",
+	        				"minecraft:sweet_berries",
+	        				"minecraft:ladder",
+	        				"minecraft:vine",
+	        				"minecraft:twisting_vines",
+	        				"minecraft:slime_block",
+	        				"minecraft:honey_block",
+	        				"minecraft:white_bed",
+	        				"minecraft:orange_bed",
+	        				"minecraft:magenta_bed",
+	        				"minecraft:light_blue_bed",
+	        				"minecraft:yellow_bed",
+	        				"minecraft:lime_bed",
+	        				"minecraft:gray_bed",
+	        				"minecraft:light_gray_bed",
+	        				"minecraft:cyan_bed",
+	        				"minecraft:purple_bed",
+	        				"minecraft:blue_bed",
+	        				"minecraft:brown_bed",
+	        				"minecraft:green_bed",
+	        				"minecraft:red_bed",
+	        				"minecraft:black_bed"));
 	        preventGettingInBoat = builder.comment("Prevent players from getting in boats while they're falling. NOTE that "
 	        		+ "boats will still cancel fall damage, so I recommend installing Random Tweaks "
 	        		+ "(https://www.curseforge.com/minecraft/mc-mods/randompatches-forge) to re-enable boat fall damage.")
